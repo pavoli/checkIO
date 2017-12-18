481d3e07735056b1183e83dbfe5c0f10e3483fc1
p.olifer <pavel.olifer@gmail.com>
2017-12-18 07:47:58 +0300

    second_index

---
+++ elementary/second_index.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+__author__ = 'p.olifer'
+
+"""
+Let's go through the first example where you need to find the second occurrence of "s" in a word "sims".
+It’s easy to find its first occurrence with a function index or
+find which will point out that "s" is the first symbol in a word "sims"
+and therefore the index of the first occurrence is 0.
+But we have to find the second "s" which is 4th in a row and
+that means that the index of the second occurrence (and the answer to a question) is 3.
+Input: Two strings.
+Output: Int or None
+"""
+
+if __name__ == '__main__':
+    pass
