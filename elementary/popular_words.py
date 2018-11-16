1457757e1a6a8afbc3eb58162ec3cc9a196ea7f0
p.olifer <pavel.olifer@gmail.com>
2018-11-16 22:55:39 +0300

    popular_words

--- elementary/popular_words.py
+++ elementary/popular_words.py
@@ -2,9 +2,20 @@
 #! /usr/local/bin/python3
 __author__ = 'p.olifer'
 
-def popular_words():
-    pass
+def popular_words(text: str, words: list) -> dict:
+    for i in text.split(' '):
+        print(i)
 
 
 if __name__ == '__main__':
-    popular_words()
+    popular_words('''
+    When I was One
+    I had just begun
+    When I was Two
+    I was nearly new
+    ''', ['i', 'was', 'three', 'near']) == {
+        'i': 4,
+        'was': 3,
+        'three': 0,
+        'near': 0
+    }
