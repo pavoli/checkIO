e69ed385514d7cd7b8e3baca4457cfcc315fa21c
p.olifer <pavel.olifer@gmail.com>
2018-11-20 07:37:28 +0300

    popular_words, finish

--- elementary/popular_words.py
+++ elementary/popular_words.py
@@ -2,27 +2,26 @@
 #! /usr/local/bin/python3
 __author__ = 'p.olifer'
 
-def popular_words(text: str, words: list) -> dict:
-    d = {a.lower():0 for a in words}
-    new_text = [i.strip('\n').lower() for i in text.split(' ') if i]
+#def popular_words(text: str, words: list) -> dict:
+def popular_words(text, words):
+    #new_text = [i.lower() for i in text.replace('\n',' ').split(' ') if i]
+    #d = {a.lower():0 for a in words}
+    #for k in d:
+    #    for i in new_text:
+    #        if k == i:
+    #
+    return {word:text.lower().split().count(word) for word in words}
 
-    for k, v in d.items():
-        for i in new_text:
-            if k == i:
-                d[k] =+ 1
 
-    for i in d:
-        print(d)
+#def popular_words(text):
+#    for i in text.strip('\n').split(' '):
+#        print i
 
 if __name__ == '__main__':
-    popular_words('''
-    When I was One
-    I had just begun
-    When I was Two
-    I was nearly new
-    ''', ['i', 'was', 'three', 'near']) == {
+    #popular_words('''\nWhen I was One\nI had just begun\nWhen I was Two\nI was nearly new\n''')
+    print (popular_words('''\nWhen I was One\nI had just begun\nWhen I was Two\nI was nearly new\n''', ['i', 'was', 'three', 'near']) == {
         'i': 4,
         'was': 3,
         'three': 0,
         'near': 0
-    }
+    })
