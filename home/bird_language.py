70489106d595d0a0541a1084b69de9cb485405a0
p.olifer <pavel.olifer@gmail.com>
2019-07-01 09:18:24 +0300

    commit all

--- home/bird_language.py
+++ home/bird_language.py
@@ -5,9 +5,19 @@ __author__ = 'p.olifer'
 
 def translate(words):
     new_words = words.split(' ')
+    print(len(new_words))
     answer = ''
     for x in new_words:
         print(x)
+        answer += x
+        if x in 'aeiouy':
+            gap = 3
+        else:
+            gap = 2
+        print('gap={}'.format(gap))
+        new_words = new_words[2:]
+        print('new_words={}'.format(new_words))
+    print(answer)
 
 if __name__ == '__main__':
     #translate("hieeelalaooo") == "hello"
