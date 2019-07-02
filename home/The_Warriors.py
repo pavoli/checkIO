cfecf23259cf206d741fdd2f6cc4db2c1cc0ae3d
p.olifer <pavel.olifer@gmail.com>
2019-07-03 00:19:11 +0300

    start learning OOP

---
+++ home/The_Warriors.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'p.olifer'
+__version__ = '1.0'
+
+
+class Warrior():
+
+    def __init__(self):
+        self.health = 50
+        self.attack = 5
+
+    def is_alive(self):
+        return True if self.health > 0 else False
+
+
+class Knight(Warrior):
+
+    def __init__(self):
+        self.health = 50
+        self.attack = 7
+
+
+def fight(unit1, unit2):
+    while unit1.health > 0 or unit2.health > 0:
+        unit2.health -= unit1.attack
+        unit1.health -= unit2.attack
+    return True if unit1.health > 0 else False
+
+if __name__ == '__name__':
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+
